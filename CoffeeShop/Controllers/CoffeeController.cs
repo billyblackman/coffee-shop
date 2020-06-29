d0614f8b1d530b6a6dd18f942e9d55e3baf010e6
Billy Blackman <blackmanbilly@gmail.com>
2020-06-29 16:05:03 -0500

    Initial commit

---
+++ CoffeeShop/Controllers/CoffeeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class CoffeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CoffeeShop/js-app/main.js
@@ -0,0 +1,26 @@
+﻿const beanVarietyUrl = "https://localhost:5001/api/beanvariety/";
+const coffeeUrl = "https://localhost:5001/api/coffee/";
+
+const beansButton = document.querySelector("#run-button");
+beansButton.addEventListener("click", () => {
+    getAllBeanVarieties()
+        .then(beanVarieties => {
+            console.log(beanVarieties);
+        })
+});
+
+const coffeeButton = document.querySelector("#coffee-button");
+beansButton.addEventListener("click", () => {
+    getAllCoffees()
+        .then(coffees => {
+            console.log(coffees);
+        })
+});
+
+function getAllBeanVarieties() {
+    return fetch(beanVarietyUrl).then(resp => resp.json());
+}
+
+function getAllCoffees() {
+    return fetch(coffeeUrl).then(resp => resp.json());
+}
